ec03cefedf2a67d5f2bd3ffc6b5d101e1e516513
ly <ly@163.com>
2020-08-09 22:58:12 -0700

    init a dsp project.
    Signed-off-by: ly <ly@163.com>

---
+++ dsp/include/DSP28_Gpio.c
@@ -0,0 +1,48 @@
+//
+//      TMDX ALPHA RELEASE
+//      Intended for product evaluation purposes
+//
+//###########################################################################
+//
+// FILE:	DSP28_Gpio.c
+//
+// TITLE:	DSP28 General Purpose I/O Initialization & Support Functions.
+//
+//###########################################################################
+//
+//  Ver | dd mmm yyyy | Who  | Description of changes
+// =====|=============|======|===============================================
+//  0.55| 06 May 2002 | L.H. | EzDSP Alpha Release
+//  0.56| 20 May 2002 | L.H. | No change
+//  0.57| 27 May 2002 | L.H. | No change
+//###########################################################################
+
+#include "DSP28_Device.h"
+
+//---------------------------------------------------------------------------
+// InitGpio: 
+//---------------------------------------------------------------------------
+// This function initializes the Gpio to a known state.
+//
+void InitGpio(void)
+{
+
+// Set GPIO A port pins,AL(Bits 7:0)(input)-AH(Bits 15:8) (output) 8bits
+// Input Qualifier =0, none
+     EALLOW;
+     GpioMuxRegs.GPAMUX.all=0x0000;     
+     GpioMuxRegs.GPADIR.all=0xFF00;    	// upper byte as output/low byte as input
+     GpioMuxRegs.GPAQUAL.all=0x0000;	// Input qualifier disabled
+
+// Set GPIO B port pins, configured as EVB signals
+// Input Qualifier =0, none
+// Set bits to 1 to configure peripherals signals on the pins
+     GpioMuxRegs.GPBMUX.all=0xFFFF;   
+     GpioMuxRegs.GPBQUAL.all=0x0000;	// Input qualifier disabled
+     EDIS;
+
+}	
+	
+//===========================================================================
+// No more.
+//===========================================================================
